f21b72dafc35d1385989bad4f94de0723d382fbf
Elias K <127874391+n2koel00@users.noreply.github.com>
2023-09-06 20:03:16 +0300

    valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState(90)
+  const [intensity, setIntensity] = useState(1.7)
+  const [gender, setGender] = useState("male")
+  const [result, setResult] = useState(0)
+
+  //function calculate()
+  const calculate = () => {
+    let calories = 0
+    if (gender === "male") {
+      calories = (879 + 10.2 * weight) *intensity
+    } else {
+      calories = (795 + 7.18 * weight) *intensity
+    }
+    setResult(calories)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Calories</h3>
+      <div>
+        <label>Weight</label>
+        <input type="number" value={weight} onChange={e => setWeight(e.target.value)}/>
+      </div>
+      <div>
+        <label>Intensity</label>
+        <select value={intensity} onChange={e => setIntensity(e.target.value)}>
+          <option value="1.3">Light</option>
+          <option value="1.5">Usual</option>
+          <option value="1.7">Moderate</option>
+          <option value="2">Hard</option>
+          <option value="2.2">Very Hard</option>
+        </select>
+      </div>
+      <div>
+        <label>Gender</label>
+        <input type='radio' name='gender' value="male" defaultChecked onChange={e => setGender(e.target.value)}/><label>Male</label>
+        <input type='radio' name='gender' value="Female" onChange={e => setGender(e.target.value)}/><label>Female</label>
+      </div>
+      <div>
+        <label>Result</label>
+        <output>{result.toFixed(0)}</output>
+      </div>
+      <div>
+        <button type='button' onClick={calculate}>Calculate</button>
+      </div>
     </div>
   );
 }
